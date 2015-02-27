71be891e7e5a9e1be17d8ea1f76ce0c4f3d4f7a0
yanyiwu <i@yanyiwu.com>
2015-02-28 01:12:04 +0800

    add cardflow.go

--- controllers/index.go
+++ controllers/index.go
@@ -2,9 +2,7 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/golang/glog"
 	"github.com/yanyiwu/infor-you-mation/models"
-	"strings"
 )
 
 type IndexController struct {
@@ -12,71 +10,6 @@ type IndexController struct {
 }
 
 func (mc *IndexController) Get() {
-	mc.Data["CardFlows"] = mc.getCardFlows()
+	mc.Data["CardFlows"] = models.GetCardFlows()
 	mc.TplNames = "index.html"
-}
-
-type CardFlow struct {
-	Cards []*Card
-}
-
-type Card struct {
-	Topic string
-	Feeds []*Feed
-}
-
-type Feed struct {
-	Title string
-	Url   string
-}
-
-const FeedsLimit = 5
-const CardFlowN = 2
-
-var Topics = [...]string{
-	"实习",
-	"兼职",
-	"设计",
-	"前端",
-	"PHP",
-	"C++",
-	"iOS",
-	"Android",
-}
-
-func (mc *IndexController) getCardFlows() []*CardFlow {
-	cf := make([]*CardFlow, CardFlowN)
-	for i := 0; i < len(cf); i++ {
-		cf[i] = &CardFlow{make([]*Card, 0)}
-	}
-	for i := 0; i < len(Topics); i++ {
-		c := mc.getCardByTopic(Topics[i])
-		if c != nil {
-			cf[i%len(cf)].Cards = append(cf[i%len(cf)].Cards, c)
-		}
-	}
-	return cf
-}
-
-func (mc *IndexController) getCardByTopic(topic string) *Card {
-	kfps, err := models.GetKeywordFeedPairs(strings.ToLower(topic), FeedsLimit)
-	if err != nil {
-		glog.Error(err)
-		return nil
-	}
-	c := Card{topic, make([]*Feed, 0)}
-	for i := 0; i < len(kfps); i++ {
-		feeds, err := models.GetFeedById(kfps[i].Feedid)
-		if err != nil {
-			glog.Error(err)
-			return nil
-		}
-		if len(feeds) != 1 {
-			glog.Error("feeds illegal!!!")
-			return nil
-		}
-		feed := Feed{feeds[0].Title, feeds[0].Url}
-		c.Feeds = append(c.Feeds, &feed)
-	}
-	return &c
 }
---
+++ models/cardflow.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"github.com/golang/glog"
+	"strings"
+)
+
+type CardFlow struct {
+	Cards []*Card
+}
+
+type Card struct {
+	Topic string
+	Feeds []*Feed
+}
+
+const FeedsLimit = 5
+const CardFlowN = 2
+
+var Topics = [...]string{
+	"实习",
+	"兼职",
+	"设计",
+	"前端",
+	"PHP",
+	"C++",
+	"iOS",
+	"Android",
+}
+
+func GetCardFlows() []*CardFlow {
+	cf := make([]*CardFlow, CardFlowN)
+	for i := 0; i < len(cf); i++ {
+		cf[i] = &CardFlow{make([]*Card, 0)}
+	}
+	for i := 0; i < len(Topics); i++ {
+		c := getCardByTopic(Topics[i])
+		if c != nil {
+			cf[i%len(cf)].Cards = append(cf[i%len(cf)].Cards, c)
+		}
+	}
+	return cf
+}
+
+func getCardByTopic(topic string) *Card {
+	kfps, err := GetKeywordFeedPairs(strings.ToLower(topic), FeedsLimit)
+	if err != nil {
+		glog.Error(err)
+		return nil
+	}
+	c := Card{topic, make([]*Feed, 0)}
+	for i := 0; i < len(kfps); i++ {
+		feeds, err := GetFeedById(kfps[i].Feedid)
+		if err != nil {
+			glog.Error(err)
+			return nil
+		}
+		if len(feeds) != 1 {
+			glog.Error("feeds illegal!!!")
+			return nil
+		}
+		c.Feeds = append(c.Feeds, &feeds[0])
+	}
+	return &c
+}
