a5303faf8472089c4786af3b74068ec9685590ab
yanyiwu <i@yanyiwu.com>
2015-03-26 19:14:44 +0800

    past day

--- models/util.go
+++ models/util.go
@@ -5,9 +5,19 @@ import (
 	"time"
 )
 
-func GetPastDayRange() bson.M {
+func GetYesterdayRange() bson.M {
 	righttime := time.Now()
 	righttime = time.Date(righttime.Year(), righttime.Month(), righttime.Day(), 0, 0, 0, 0, time.UTC)
+	d, err := time.ParseDuration("-24h")
+	if err != nil {
+		panic(err)
+	}
+	lefttime := righttime.Add(d)
+	return bson.M{"$gte": lefttime, "$lt": righttime}
+}
+
+func GetPastDayRange() bson.M {
+	righttime := time.Now()
 	d, err := time.ParseDuration("-24h")
 	if err != nil {
 		panic(err)
