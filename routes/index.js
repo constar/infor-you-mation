876bb6a4181cbd79642efab1bbe7f289e36ee767
lucky7ky <kaiyi0707@163.com>
2015-05-31 23:28:15 +0800

    practice

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { powerBy: 'Express' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { powerBy: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login', { powerBy: 'Express'});
+})
 module.exports = router;
