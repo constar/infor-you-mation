6058c4ee88fbd669f7262a69587f47c371bcf0ba
lucky7ky <kaiyi0707@163.com>
2015-06-16 00:27:17 +0800

    add nodejieba

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var nodejieba = require('nodejieba');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,5 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { powerBy: 'Express' });
 });
 
+router.get('/cut/:sentence', function(req, res, next) {
+    var words = nodejieba.cut(req.params.sentence);
+    console.log(words);
+    res.send(words);
+});
 module.exports = router;
 
