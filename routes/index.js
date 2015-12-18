a86b544abd0ee33ceeed97967842b67bb127366f
lucky7ky <kaiyi0707@163.com>
2015-12-18 23:59:39 +0800

    redis

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+client = redis.createClient();
+
+//var data = [{
+        //'keyword':'PHP',
+        //'arrs':[{
+            //'text':'标题1',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题2',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题3',
+            //'url':'http://baidu.com'
+        //}]
+    //},{
+        //'keyword':'Java',
+        //'arrs':[{
+            //'text':'标题1',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题2',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题3',
+            //'url':'http://baidu.com'
+        //}]
+    //},{
+        //'keyword':'CSS',
+        //'arrs':[{
+            //'text':'标题1',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题2',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题3',
+            //'url':'http://baidu.com'
+        //},{
+            //'text':'标题4',
+            //'url':'http://baidu.com'
+        //}]
+//}];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 
 router.get('/data', function(req, res, next) {
-    res.json([{
-        'keyword':'PHP',
-        'arrs':[{
-            'text':'标题1',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题2',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题3',
-            'url':'http://baidu.com'
-        }]
-    },{
-        'keyword':'Java',
-        'arrs':[{
-            'text':'标题1',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题2',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题3',
-            'url':'http://baidu.com'
-        }]
-    },{
-        'keyword':'CSS',
-        'arrs':[{
-            'text':'标题1',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题2',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题3',
-            'url':'http://baidu.com'
-        },{
-            'text':'标题4',
-            'url':'http://baidu.com'
-        }]
-    }]
-   );
+    client.get('data', function(err, reply) {
+        res.json(JSON.parse(reply));
+        console.log(reply);
+    })
 });
 
 //router.get('/:path', function(req, res, next) {
