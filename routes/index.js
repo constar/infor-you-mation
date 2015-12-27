89b020cc953a5249aeee6d2d65721ad5c81a5708
luckykaiyi <kaiyi0707@163.com>
2015-12-27 22:41:15 +0800

    promise

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var redis = require('redis');
+var Promise = require('bluebird');
 client = redis.createClient();
 
 //var data = [{
@@ -51,13 +52,60 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/data', function(req, res, next) {
-    client.get('data', function(err, reply) {
-        res.json(JSON.parse(reply));
-        console.log(reply);
+    var config = 10;
+    var names = [];
+    var joblists = [];
+    var jobIds =[];
+    var data = [];
+    for (var i = 1; i <= config; i++) {
+        names.push('topic:' + i + ':name');
+        joblists.push(getJobIds('topic:' + i + ':joblist'));
+    }
+    function getJobIds(keyname) {
+        return new Promise(function(resolve, reject) {
+            client.zrange(keyname, 0, -1, function(err, reply) {
+                resolve(reply);
+            })
+        })
+    } 
+    Promise.all(joblists).then(function(value){
+        jobIds = value;
+        Promise.all([getJobTopics(), getJobTitles()]).then(function(){
+            res.json(data);
+        })
     })
-});
+    function getJobTopics() {
+        return new Promise(function(resolve, reject) {
+            client.mget(names, function(err, reply){
+                for (var i = 0; i < reply.length; i++) {
+                    data.push({'topic':reply[i]});
+                } 
+                resolve();
+            })
+        })
+    }
+    function getJobTitles() {
+        return new Promise(function(resolve, reject) {
+            var jobIdKeyNames = [];
+            var count = 0;
+            for (var i = 0; i < jobIds.length; i++) {
+                for (var j = 0; j < jobIds[i].length; j++) {
+                    jobIdKeyNames.push('job:' + jobIds[i][j] + ':title');
+                }
+                client.mget(jobIdKeyNames, function(err, reply) {
+                    for (var i = 0; i < reply.length; i++) {
+                        reply[i] = {'title':reply[i]};
+                    }
+                    data[count++].jobs = reply;
+                })
+                jobIdKeyNames = [];
+            }
+            resolve();
+        })
+    }
 
 //router.get('/:path', function(req, res, next) {
   //res.render(req.params.path);
 //});
+});
 module.exports = router;
