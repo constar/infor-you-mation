0a8f4ad94e84eb3521872fafdc4314abfe7dab31
luckykaiyi <kaiyi0707@163.com>
2016-01-15 18:05:24 +0800

    single page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,22 @@
-var app = angular.module("infor-you-mation",[]);
+var app = angular.module("infor-you-mation",['ngRoute']);
+app.config(['$routeProvider',
+    function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl:'/pages/index.html',
+        controller:'indexCtrl'
+    })
+    .when('/register', {
+        templateUrl:'/pages/register.html',
+        controller:'registerCtrl'
+    });
+}]);
 app.controller("indexCtrl", function($scope, $http){
-        $http.get('/data')
-        .success(function(res){
-            $scope.lists = res;
-        })
+    $http.get('/data')
+    .success(function(res){
+        $scope.lists = res;
+    })
+});
+app.controller("registerCtrl", function($scope){
 });
 
