b8188c0edf3a763f2d9d055e84a0b61bad12dc37
lucky7ky <kaiyi0707@163.com>
2016-01-19 17:29:51 +0800

    ...

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,13 +6,17 @@ app.config(['$routeProvider',
         templateUrl:'/pages/index.html',
         controller:'indexCtrl'
     })
+    .when('/register', {
+        templateUrl:'/pages/register.html',
+        controller:'registerCtrl'
+    })
     .when('/login', {
         templateUrl:'/pages/login.html',
         controller:'loginCtrl'
     })
-    .when('/register', {
-        templateUrl:'/pages/register.html',
-        controller:'registerCtrl'
+    .when('/home', {
+        templateUrl:'/pages/home.html',
+        controller:'homeCtrl'
     });
 }]);
 app.controller("indexCtrl", function($scope, $http){
@@ -24,5 +28,7 @@ app.controller("indexCtrl", function($scope, $http){
 app.controller("registerCtrl", function($scope){
 });
 app.controller("loginCtrl", function($scope){
+});
+app.controller("homeCtrl", function($scope){
 });
 
